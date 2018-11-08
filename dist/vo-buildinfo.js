598f7579d7651c45493a2d675fc6ce9168921ee9
Tom Coemans <tom.coemans@gmail.com>
2018-11-08 09:28:40 +0100

    GVR-39 initiële commit

---
+++ dist/vo-buildinfo.js
@@ -0,0 +1,19 @@
+import { LitElement, html } from "@polymer/lit-element/lit-element";
+
+/**
+ * `vo-buildversie`
+ * Visualiseert de build versie van de applicatie.
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/index.html
+ */
+class VoBuildversie extends LitElement {
+  render() {
+    return html`
+			
+		`;
+  }
+}
+
+customElements.define('vo-buildversie', VoBuildversie);
---
+++ vo-buildinfo.js
@@ -0,0 +1,19 @@
+import { LitElement, html } from "./assets/@polymer/lit-element/lit-element.js";
+
+/**
+ * `vo-buildversie`
+ * Visualiseert de build versie van de applicatie.
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/index.html
+ */
+class VoBuildversie extends LitElement {
+	render() {
+		return html`
+			
+		`;
+	}
+}
+
+customElements.define('vo-buildversie', VoBuildversie);
